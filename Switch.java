d15fa8f36c36e8af048d280b0244fc9806e179e6
Sandesh Shrestha <yoursandeshgeneral@gmail.com>
2024-10-03 21:59:44 +0530

    chore: Sandesh is Switching at Java

---
+++ Switch.java
@@ -0,0 +1,16 @@
+public class Switch {
+    public static void main(String[] args) {
+        int day = 1;
+        switch (day) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            default:
+                System.out.println("Invalid day");
+        }
+        
+    }
+}
