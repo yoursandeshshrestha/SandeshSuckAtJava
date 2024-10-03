a6d5f65ced7cb92e626acb47a2812a5c79cb7878
Sandesh Shrestha <yoursandeshgeneral@gmail.com>
2024-10-03 21:57:00 +0530

    if else

---
+++ IfElse.java
@@ -0,0 +1,10 @@
+public class IfElse {
+    public static void main(String[] args) {
+        int age = 18;
+        if(age >= 18){
+            System.out.println("You are an adult");
+        }else{
+            System.out.println("You are an minor");
+        }
+    }
+}
