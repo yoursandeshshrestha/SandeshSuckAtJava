7d83f551945502310d4e76356259b8189405b684
Sandesh Shrestha <yoursandeshgeneral@gmail.com>
2024-10-03 23:53:20 +0530

    chore: Methoding the method

---
+++ Methoding.java
@@ -0,0 +1,10 @@
+public class Methoding {
+    public static void main(String[] args) {
+        greet();
+    }
+        public static void greet() {
+            System.out.println("Hello, World!");
+        }
+        
+    
+}
