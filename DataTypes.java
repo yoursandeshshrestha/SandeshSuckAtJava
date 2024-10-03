1c8813f6d98405aa3edebb277b2c438048bc67bd
Sandesh Shrestha <yoursandeshgeneral@gmail.com>
2024-10-03 21:56:54 +0530

    Data Types

---
+++ DataTypes.java
@@ -0,0 +1,16 @@
+// Data types are divided into two groups:
+
+// Primitive data types - includes byte, short, int, long, float, double, boolean and char
+// Non-primitive data types - such as String, Arrays and Classes (you will learn more about these in a later chapter)
+
+class DataTypes {
+    public static void main(String[] args) {
+        String name = "Sandesh Shrestha";
+        int age = 19;
+        boolean adult = true;
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(adult);
+        System.out.println("My name is " + name + " and i'm " + age + " year old");
+    }
+}
